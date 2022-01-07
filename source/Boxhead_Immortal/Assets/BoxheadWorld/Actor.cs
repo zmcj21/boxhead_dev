88137822c4bd723063b19c1da62ccac051c06ef6
zmcj21 <zmcj20plusplus@gmail.com>
2022-01-07 11:57:46 +0300

    add scripts

---
+++ source/Boxhead_Immortal/Assets/BoxheadWorld/Actor.cs
@@ -0,0 +1,396 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public enum ActorType
+{
+    Player = 1,
+    Monster,
+}
+
+public enum ActorDirection
+{
+    None = 0,
+    Up,
+    Down,
+    Left,
+    Right,
+    LeftTop,
+    LeftDown,
+    RightTop,
+    RightDown,
+}
+
+public abstract class Actor : BObject
+{
+    private ActorType actorType;
+
+    private ActorDirection actorDirection;
+
+    private float moveSpeed;
+
+    private float curSpeed;
+
+    private GameObject actorObject;
+
+    private Animator actorAnimator;
+
+    private Rigidbody actorRigidbody;
+
+    private CapsuleCollider actorCollider;
+
+    private bool isPrimitive; //private only
+
+    private bool isStiff;
+
+    public ActorType ActorType
+    {
+        get => actorType;
+        set => actorType = value;
+    }
+
+    public ActorDirection ActorDirection
+    {
+        get => actorDirection;
+        set => actorDirection = value;
+    }
+
+    public float MoveSpeed
+    {
+        get => moveSpeed;
+        set => moveSpeed = value;
+    }
+
+    public float CurSpeed
+    {
+        get => curSpeed;
+        set => curSpeed = value;
+    }
+
+    public GameObject ActorObject
+    {
+        get => actorObject;
+    }
+
+    public Animator ActorAnimator
+    {
+        get => actorAnimator;
+    }
+
+    public Rigidbody ActorRigidbody
+    {
+        get => actorRigidbody;
+    }
+
+    public CapsuleCollider ActorCollider
+    {
+        get => actorCollider;
+    }
+
+    //是否僵硬, 通常被攻击后会进入僵硬时间, 期间无法操控该单位
+    public bool IsStiff
+    {
+        get => isStiff;
+        set => isStiff = value;
+    }
+
+    public void InitActor(ActorType actorType, float maxHealth, ActorDirection dir, float moveSpeed, string actorPrefabPath)
+    {
+        InitBObject(BObjectType.Actor, maxHealth);
+
+        ActorType = actorType;
+        ActorDirection = dir;
+        MoveSpeed = moveSpeed;
+        CurSpeed = MoveSpeed; //当前速度为移动速度
+
+        RotateActor(dir);
+
+        string actorPrefabName = null;
+        int leftSlashPos = actorPrefabPath.LastIndexOf('/');
+        int rightSlashPos = actorPrefabPath.LastIndexOf('\\');
+        if (leftSlashPos != -1)
+        {
+            actorPrefabName = actorPrefabPath.Substring(leftSlashPos + 1, actorPrefabPath.Length - leftSlashPos - 1);
+        }
+        else if (rightSlashPos != -1)
+        {
+            actorPrefabName = actorPrefabPath.Substring(rightSlashPos + 1, actorPrefabPath.Length - rightSlashPos - 1);
+        }
+        //检测自身是否挂载了模型, 如果已经挂载了模型则不动态加载模型:
+        Transform child = transform.Find(actorPrefabName);
+        if (child && child.gameObject.activeInHierarchy)
+        {
+            actorObject = child.gameObject;
+            actorAnimator = actorObject.GetComponent<Animator>();
+            Debug.Log("Find Model Success.");
+            if (!actorAnimator)
+            {
+                Debug.LogError("Missing Animator");
+            }
+        }
+        else
+        {
+            //如果能够加载预制体则实例化它
+            GameObject actorPrefab = Resources.Load<GameObject>(actorPrefabPath);
+            if (actorPrefab)
+            {
+                actorObject = Instantiate(actorPrefab, Vector3.zero, Quaternion.identity, transform);
+                actorAnimator = actorObject.GetComponent<Animator>();
+                Debug.Log("Load Model Success.");
+                if (!actorAnimator)
+                {
+                    Debug.LogError("Missing Animator");
+                }
+            }
+            //加载失败则创建一个胶囊体当作占位符, 但是没有Animator, 播放动画时会出现警告
+            else
+            {
+                actorObject = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                actorAnimator = null;
+                isPrimitive = true;
+                actorObject.transform.parent = transform;
+                actorObject.transform.localPosition = new Vector3(0, 1, 0);
+                Collider useless_collider = actorObject.GetComponent<Collider>();
+                Destroy(useless_collider);
+                Debug.LogWarning("Load Model Error, now I will add a capsule for you.");
+            }
+        }
+
+        actorRigidbody = GetComponent<Rigidbody>();
+        actorCollider = GetComponent<CapsuleCollider>();
+
+        if (!actorRigidbody)
+        {
+            Debug.LogWarning("Missing Rigidbody, Now I will add default one.");
+            actorRigidbody = gameObject.AddComponent<Rigidbody>();
+            actorRigidbody.mass = 1;
+            actorRigidbody.drag = 0;
+            actorRigidbody.angularDrag = 0;
+            actorRigidbody.useGravity = true;
+            actorRigidbody.isKinematic = false;
+            actorRigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+            actorRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            actorRigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+        }
+
+        if (!actorCollider)
+        {
+            Debug.LogWarning("Missing Collider, Now I will add default one.");
+            actorCollider = gameObject.AddComponent<CapsuleCollider>();
+            actorCollider.isTrigger = false;
+            actorCollider.material = null; //todo
+            actorCollider.center = new Vector3(0, 1, 0);
+            actorCollider.radius = 0.5f;
+            actorCollider.height = 2;
+            actorCollider.direction = 1; //X=0, Y=1, Z=2
+        }
+    }
+
+    public void RotateActor(ActorDirection dir)
+    {
+        ActorDirection = dir;
+
+        switch (dir)
+        {
+            case ActorDirection.Up:
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                break;
+            case ActorDirection.Down:
+                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                break;
+            case ActorDirection.Left:
+                transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+                break;
+            case ActorDirection.Right:
+                transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+                break;
+            case ActorDirection.LeftTop:
+                transform.rotation = Quaternion.Euler(new Vector3(0, -45, 0));
+                break;
+            case ActorDirection.LeftDown:
+                transform.rotation = Quaternion.Euler(new Vector3(0, -135, 0));
+                break;
+            case ActorDirection.RightTop:
+                transform.rotation = Quaternion.Euler(new Vector3(0, 45, 0));
+                break;
+            case ActorDirection.RightDown:
+                transform.rotation = Quaternion.Euler(new Vector3(0, 135, 0));
+                break;
+        }
+    }
+
+    public void AnimatorPlaySafe(string clipName)
+    {
+        if (!actorAnimator)
+        {
+            if (!isPrimitive)
+            {
+                Debug.LogWarning("AnimatorPlaySafe failed, can't find Animator component!");
+            }
+            return;
+        }
+        actorAnimator.Play(clipName);
+    }
+
+    protected override void WhenImHurt(float damage, ExtraHitInfo hitInfo)
+    {
+        IEnumerator ProcessStiff()
+        {
+            IsStiff = true;
+            yield return new WaitForSeconds(0.1f);
+            IsStiff = false;
+        }
+        StartCoroutine(ProcessStiff());
+    }
+
+    protected override void WhenImDead(ExtraHitInfo hitInfo)
+    {
+        Destroy(gameObject);
+    }
+
+    public static ActorDirection GetOppositeDirection(ActorDirection dir)
+    {
+        switch (dir)
+        {
+            case ActorDirection.Up:
+                return ActorDirection.Down;
+            case ActorDirection.Down:
+                return ActorDirection.Up;
+            case ActorDirection.Left:
+                return ActorDirection.Right;
+            case ActorDirection.Right:
+                return ActorDirection.Left;
+            case ActorDirection.LeftTop:
+                return ActorDirection.RightDown;
+            case ActorDirection.LeftDown:
+                return ActorDirection.RightTop;
+            case ActorDirection.RightTop:
+                return ActorDirection.LeftDown;
+            case ActorDirection.RightDown:
+                return ActorDirection.LeftTop;
+            default:
+                return ActorDirection.None;
+        }
+    }
+
+    public static System.Tuple<ActorDirection, ActorDirection> GetHorizontalDirections(ActorDirection dir)
+    {
+        System.Tuple<ActorDirection, ActorDirection> tuple;
+
+        switch (dir)
+        {
+            case ActorDirection.Up:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.Left, ActorDirection.Right);
+                break;
+            case ActorDirection.Down:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.Right, ActorDirection.Left);
+                break;
+            case ActorDirection.Left:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.Down, ActorDirection.Up);
+                break;
+            case ActorDirection.Right:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.Up, ActorDirection.Down);
+                break;
+            case ActorDirection.LeftTop:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.LeftDown, ActorDirection.RightTop);
+                break;
+            case ActorDirection.LeftDown:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.RightDown, ActorDirection.LeftTop);
+                break;
+            case ActorDirection.RightTop:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.LeftTop, ActorDirection.RightDown);
+                break;
+            case ActorDirection.RightDown:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.RightTop, ActorDirection.LeftDown);
+                break;
+            default:
+                tuple = new System.Tuple<ActorDirection, ActorDirection>(ActorDirection.None, ActorDirection.None);
+                break;
+        }
+
+        return tuple;
+    }
+
+    public static System.Tuple<ActorDirection, ActorDirection, ActorDirection> GetBackwardDirections(ActorDirection dir)
+    {
+        System.Tuple<ActorDirection, ActorDirection, ActorDirection> tuple;
+
+        switch (dir)
+        {
+            case ActorDirection.Up:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.LeftDown, ActorDirection.Down, ActorDirection.RightDown);
+                break;
+            case ActorDirection.Down:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.RightTop, ActorDirection.Up, ActorDirection.LeftTop);
+                break;
+            case ActorDirection.Left:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.RightDown, ActorDirection.Right, ActorDirection.RightTop);
+                break;
+            case ActorDirection.Right:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.LeftTop, ActorDirection.Left, ActorDirection.LeftDown);
+                break;
+            case ActorDirection.LeftTop:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.Down, ActorDirection.RightDown, ActorDirection.Right);
+                break;
+            case ActorDirection.LeftDown:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.Right, ActorDirection.RightTop, ActorDirection.Up);
+                break;
+            case ActorDirection.RightTop:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.Left, ActorDirection.LeftDown, ActorDirection.Down);
+                break;
+            case ActorDirection.RightDown:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.Up, ActorDirection.LeftTop, ActorDirection.Left);
+                break;
+            default:
+                tuple = new System.Tuple<ActorDirection, ActorDirection, ActorDirection>(ActorDirection.None, ActorDirection.None, ActorDirection.None);
+                break;
+        }
+
+        return tuple;
+    }
+
+    public static ActorDirection GetActorDirection(Vector2 vector)
+    {
+        ActorDirection dir = ActorDirection.None;
+
+        if (vector.x == 0 && vector.y == 0)
+        {
+            dir = ActorDirection.None;
+        }
+        else if (vector.x < 0 && vector.y > 0)
+        {
+            dir = ActorDirection.LeftTop;
+        }
+        else if (vector.x > 0 && vector.y > 0)
+        {
+            dir = ActorDirection.RightTop;
+        }
+        else if (vector.x < 0 && vector.y < 0)
+        {
+            dir = ActorDirection.LeftDown;
+        }
+        else if (vector.x > 0 && vector.y < 0)
+        {
+            dir = ActorDirection.RightDown;
+        }
+        else if (vector.x > 0)
+        {
+            dir = ActorDirection.Right;
+        }
+        else if (vector.x < 0)
+        {
+            dir = ActorDirection.Left;
+        }
+        else if (vector.y > 0)
+        {
+            dir = ActorDirection.Up;
+        }
+        else if (vector.y < 0)
+        {
+            dir = ActorDirection.Down;
+        }
+
+        return dir;
+    }
+}
---
+++ source/Boxhead_Immortal/Assets/BoxheadWorld/PlayerBase.cs
@@ -0,0 +1,290 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public enum LocalPlayerID
+{
+    Player1 = 1,
+    Player2 = 2,
+    Player3 = 3,
+    Player4 = 4,
+}
+
+public enum PlayerInputKey
+{
+    Unknown = 0,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    Shoot,
+    Use,
+    NextWeapon,
+    PrevWeapon,
+    Strafe,
+}
+
+public enum PlayerState
+{
+    Dead = 1,
+    Idle,
+    Walk,
+    Strafe,
+    Stiff,
+}
+
+public abstract class PlayerBase : Actor
+{
+    private LocalPlayerID localPlayerID;
+
+    private Dictionary<PlayerInputKey, KeyCode> inputKeys;
+
+    private float horizontalStrafeMoveSpeed;
+
+    private float oppositeStrafeMoveSpeed;
+
+    private float forwardStrafeMoveSpeed;
+
+    private float backwardStrafeMoveSpeed;
+
+    private PlayerState playerState;
+
+    private Vector2 inputVector;
+
+    private bool isMoving;
+
+    public LocalPlayerID LocalPlayerID
+    {
+        get => localPlayerID;
+        set => localPlayerID = value;
+    }
+
+    public Dictionary<PlayerInputKey, KeyCode> InputKeys
+    {
+        get => inputKeys;
+    }
+
+    public float HorizontalStrafeMoveSpeed
+    {
+        get => horizontalStrafeMoveSpeed;
+        set => horizontalStrafeMoveSpeed = value;
+    }
+
+    public float OppositeStrafeMoveSpeed
+    {
+        get => oppositeStrafeMoveSpeed;
+        set => oppositeStrafeMoveSpeed = value;
+    }
+
+    public float ForwardStrafeMoveSpeed
+    {
+        get => forwardStrafeMoveSpeed;
+        set => forwardStrafeMoveSpeed = value;
+    }
+
+    public float BackwardStrafeMoveSpeed
+    {
+        get => backwardStrafeMoveSpeed;
+        set => backwardStrafeMoveSpeed = value;
+    }
+
+    public PlayerState PlayerState
+    {
+        get => playerState;
+        set => playerState = value;
+    }
+
+    public Vector2 InputVector
+    {
+        get => inputVector;
+        set => inputVector = value;
+    }
+
+    public bool IsMoving
+    {
+        get => isMoving;
+        set => isMoving = value;
+    }
+
+    private void Awake()
+    {
+        InitDefaultInputKeys();
+    }
+
+    private void InitDefaultInputKeys()
+    {
+        inputKeys = new Dictionary<PlayerInputKey, KeyCode>();
+        InputKeys.Add(PlayerInputKey.MoveUp, KeyCode.W);
+        InputKeys.Add(PlayerInputKey.MoveDown, KeyCode.S);
+        InputKeys.Add(PlayerInputKey.MoveLeft, KeyCode.A);
+        InputKeys.Add(PlayerInputKey.MoveRight, KeyCode.D);
+        InputKeys.Add(PlayerInputKey.Shoot, KeyCode.J);
+        InputKeys.Add(PlayerInputKey.Use, KeyCode.H);
+        InputKeys.Add(PlayerInputKey.NextWeapon, KeyCode.L);
+        InputKeys.Add(PlayerInputKey.PrevWeapon, KeyCode.K);
+        InputKeys.Add(PlayerInputKey.Strafe, KeyCode.Space);
+    }
+
+    public void InitPlayerBase(LocalPlayerID localPlayerID, float maxHealth, ActorDirection dir, float moveSpeed, string actorPrefabPath, float horizontalStrafeMoveSpeed, float oppositeStrafeMoveSpeed, float forwardStrafeMoveSpeed, float backwardStrafeMoveSpeed)
+    {
+        InitActor(ActorType.Player, maxHealth, dir, moveSpeed, actorPrefabPath);
+
+        LocalPlayerID = localPlayerID;
+
+        HorizontalStrafeMoveSpeed = horizontalStrafeMoveSpeed;
+        OppositeStrafeMoveSpeed = oppositeStrafeMoveSpeed;
+        ForwardStrafeMoveSpeed = forwardStrafeMoveSpeed;
+        BackwardStrafeMoveSpeed = backwardStrafeMoveSpeed;
+
+        PlayerState = PlayerState.Idle;
+    }
+
+    public Vector2 ReceiveMoveInput(out bool isMoving)
+    {
+        Vector2 moveInput = Vector2.zero;
+
+        if (Input.GetKey(InputKeys[PlayerInputKey.MoveLeft]))
+        {
+            moveInput.x--;
+        }
+        if (Input.GetKey(InputKeys[PlayerInputKey.MoveRight]))
+        {
+            moveInput.x++;
+        }
+        if (Input.GetKey(InputKeys[PlayerInputKey.MoveUp]))
+        {
+            moveInput.y++;
+        }
+        if (Input.GetKey(InputKeys[PlayerInputKey.MoveDown]))
+        {
+            moveInput.y--;
+        }
+
+        isMoving = moveInput.x != 0 || moveInput.y != 0;
+
+        return moveInput;
+    }
+
+    public bool ReceiveStrafeInput()
+    {
+        return Input.GetKey(InputKeys[PlayerInputKey.Strafe]);
+    }
+
+    public PlayerState UpdatePlayerState(bool isMoving, bool strafe)
+    {
+        if (IsDead)
+        {
+            PlayerState = PlayerState.Dead;
+        }
+        else if (IsStiff)
+        {
+            PlayerState = PlayerState.Stiff;
+        }
+        else
+        {
+            if (isMoving)
+            {
+                if (strafe)
+                {
+                    PlayerState = PlayerState.Strafe;
+                }
+                else
+                {
+                    PlayerState = PlayerState.Walk;
+                }
+            }
+            else
+            {
+                PlayerState = PlayerState.Idle;
+            }
+        }
+
+        return PlayerState;
+    }
+
+    public void SetPlayerMoveSpeed(PlayerState state, ActorDirection dir)
+    {
+        if (state == PlayerState.Dead || state == PlayerState.Idle || state == PlayerState.Stiff)
+        {
+            CurSpeed = 0;
+        }
+        else if (state == PlayerState.Walk)
+        {
+            CurSpeed = MoveSpeed;
+        }
+        else if (state == PlayerState.Strafe)
+        {
+            System.Tuple<ActorDirection, ActorDirection> horizontalDirs = Actor.GetHorizontalDirections(ActorDirection);
+            ActorDirection oppDir = Actor.GetOppositeDirection(ActorDirection);
+            System.Tuple<ActorDirection, ActorDirection, ActorDirection> backwardDirs = Actor.GetBackwardDirections(ActorDirection);
+
+            //水平移动时速度:
+            if (dir == horizontalDirs.Item1 || dir == horizontalDirs.Item2)
+            {
+                CurSpeed = HorizontalStrafeMoveSpeed;
+            }
+            //反方向移动时速度:
+            else if (dir == oppDir)
+            {
+                CurSpeed = OppositeStrafeMoveSpeed;
+            }
+            //向斜后方向移动时速度:
+            else if (dir == backwardDirs.Item1 || dir == backwardDirs.Item3)
+            {
+                CurSpeed = BackwardStrafeMoveSpeed;
+            }
+            //向前方移动时速度:
+            else if (dir == ActorDirection)
+            {
+                CurSpeed = MoveSpeed;
+            }
+            //向斜前方移动时速度:
+            else
+            {
+                CurSpeed = ForwardStrafeMoveSpeed;
+            }
+        }
+    }
+
+    public void RotatePlayerBase(PlayerState state, ActorDirection dir)
+    {
+        if (state == PlayerState.Dead) return;
+        if (state == PlayerState.Idle) return;
+        if (state == PlayerState.Strafe) return;
+        if (state == PlayerState.Stiff) return;
+
+        if (dir == ActorDirection.None) return;
+
+        RotateActor(dir);
+    }
+
+    private void Start()
+    {
+        StartPlayer();
+    }
+
+    private void Update()
+    {
+        InputVector = ReceiveMoveInput(out this.isMoving);
+        bool strafe = ReceiveStrafeInput();
+
+        ActorDirection targetDir = Actor.GetActorDirection(InputVector);
+        UpdatePlayerState(isMoving, strafe);
+
+        RotatePlayerBase(PlayerState, targetDir);
+        SetPlayerMoveSpeed(PlayerState, targetDir);
+
+        UpdatePlayer(Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 vel = new Vector3(InputVector.x, 0, InputVector.y).normalized * CurSpeed;
+        this.ActorRigidbody.velocity = vel;
+    }
+
+    protected abstract void StartPlayer();
+
+    protected abstract void UpdatePlayer(float deltaTime);
+}
